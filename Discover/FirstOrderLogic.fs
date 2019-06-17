959383719ea50980c36953747a9f2a8f04c83617
Brian Berns <brianberns@gmail.com>
2019-06-16 23:42:21 -0400

    Work on first-order logic.

---
+++ Discover/FirstOrderLogic.fs
@@ -0,0 +1,52 @@
+﻿namespace Discover
+
+// http://www.cs.jhu.edu/~phi/ai/slides/lecture-first-order-logic.pdf
+
+type Arity = uint32
+type Name = string
+
+type Function = Function of Name * Arity     // produces an "object"
+type Predicate = Predicate of Name * Arity   // produces a Boolean
+type Variable = Variable of Name
+
+type Term =
+    | Variable of Name
+    | Constant of Name
+    | Application of Function * List<Term>
+
+type Formula =
+
+        // atomic (no sub-formulas)
+    | IsTrue of Predicate * List<Term>
+    | Equality of Term * Term
+
+        // negation
+    | Negate of Formula
+
+        // binary connectives
+    | And of Formula * Formula
+    | Or of Formula * Formula
+    | Implication of Formula * Formula
+    | Biconditional of Formula * Formula
+
+        // quantifiers
+    | Exists of Variable * Formula
+    | ForAll of Variable * Formula
+
+// http://www.math.ubc.ca/~cytryn/teaching/scienceOneF10W11/handouts/OS.proof.3inference.html
+
+type InferenceRule =
+    Formula (*input template*) * Formula (*output template*)
+
+module InferenceRule =
+
+    /// (P -> Q) & P => Q
+    let modusPonens : InferenceRule =
+        let formula name =
+            IsTrue (Predicate (name, 0u), [])
+        let P = formula "P"
+        let Q = formula "Q"
+        And (Implication (P, Q), P), Q
+
+    let apply ((inputTemplate, outputTemplate) : InferenceRule) (formula : Formula) =
+        formula
--- Discover/Program.fs
+++ Discover/Program.fs
@@ -1,96 +1,22 @@
-﻿type BasicSignature =
-    | BasicSignature of string
+﻿namespace Discover
 
-type Type =
-    | BasicType of BasicSignature
-    | FunctionType of FunctionSignature
-and FunctionSignature =
-    {
-        InputType : Type
-        OutputType : Type
-    }
+module Program =
 
-type BasicValue =
-    {
-        Signature : BasicSignature
-        Value : string
-    }
+    [<EntryPoint>]
+    let main argv =
 
-type Value =
-    | BasicValue of BasicValue
-    | FunctionValue of Function
-    member this.Type =
-        match this with
-            | BasicValue basicValue -> BasicType basicValue.Signature
-            | FunctionValue func -> FunctionType func.Signature
-and Function =
-    {
-        Signature : FunctionSignature
-        Name : string
-        Implementation : Value -> Value
-    }
+        let IsMan = Predicate ("IsMan", 1u)
+        let IsMortal = Predicate ("IsMortal", 1u)
+        let x = Variable "x"
+        let formula =
+            And (
+                Implication (
+                    IsTrue (IsMan, [x]),
+                    IsTrue (IsMortal, [x])),
+                IsTrue (IsMan, [x]))
+        let result =
+            InferenceRule.apply InferenceRule.modusPonens formula
+        printfn "%A" formula
+        printfn "%A" result
 
-module Function =
-
-    let apply (input : Value) func =
-        assert(input.Type = func.Signature.InputType)
-        func.Implementation(input)
-
-    let (|.>) = apply
-
-    let compose func1 func2 =
-        {
-            Signature =
-                {
-                    InputType = func1.Signature.InputType
-                    OutputType = func2.Signature.OutputType
-                }
-            Name = sprintf "%s+%s" func1.Name func2.Name
-            Implementation = fun input ->
-                input
-                    |.> func1
-                    |.> func2
-        }
-
-type Package =
-    {
-        Values : Value[]
-        Functions : Function[]
-    }
-
-module Natural =
-
-    let zero =
-        BasicValue {
-            Signature = BasicSignature "Natural"
-            Value = "0"
-        }
-
-    let typ = zero.Type
-
-    let succ =
-        {
-            Signature =
-                {
-                    InputType = typ
-                    OutputType = typ
-                }
-            Name = "Successor"
-            Implementation = function
-                | BasicValue input ->
-                    BasicValue {
-                        input with
-                            Value = sprintf "S(%s)" input.Value
-                    }
-                | FunctionValue _ -> failwith "Unexpected"
-        }
-
-    let package =
-        {
-            Values = [| zero |]
-            Functions = [| succ |]
-        }
-
-[<EntryPoint>]
-let main argv =
-    0
+        0
