b855e5cbbc3c727d1134656609931f85cc128078
Brian Berns <brianberns@gmail.com>
2019-07-01 01:51:22 -0400

    Commented, minor renaming.

--- Discover/Unify.fs
+++ Discover/Unify.fs
@@ -21,12 +21,16 @@ type Substitution =
 
 module Substitution =
 
+    /// Creates a substitution of the given term for the
+    /// given variable.
     let create variable term =
         {
             Variable = variable
             Term = term
         }
 
+    /// Applies the given substitutions in order to the
+    /// given term.
     let apply term subs =
         (term, subs)
             ||> Seq.fold (fun acc sub ->
@@ -35,21 +39,30 @@ module Substitution =
                     sub.Term
                     acc)
 
-module Resolution =
+module Unfiy =
 
-    let rec private unifyTerms term1 term2 subs =
+    /// Tries to unify the given terms by adding to the given
+    /// substitutions.
+    let rec private tryUnifyTerms term1 term2 subs =
 
+            // apply substitions found so far
         let term1' = subs |> Substitution.apply term1
         let term2' = subs |> Substitution.apply term2
+
+            // if terms match, we've succeeded
         if term1' = term2' then
             Some subs
         else
-
+                // tries to add a new substitution (in reverse order)
             let add variable term =
+
+                    // check for variable name conflict
                 let occurs =
                     term
                         |> Term.getVariables
                         |> Set.contains variable
+
+                    // create and remember the new substitution
                 if occurs then None
                 else
                     Substitution.create variable term
@@ -62,22 +75,24 @@ module Resolution =
                 | Term variable, term -> add variable term
                 | term, Term variable -> add variable term
 
-                    // recurse
+                    // recurse on subterms
                 | Application (function1, terms1),
-                    Application (function2, terms2) ->
+                  Application (function2, terms2) ->
                     if function1 = function2 then
-                        unifyTermArrays terms1 terms2 subs
+                        tryUnifyTermArrays terms1 terms2 subs
                     else None
 
-    and private unifyTermArrays terms1 terms2 subs =
+    /// Tries to unify the given arrays of terms.
+    and private tryUnifyTermArrays terms1 terms2 subs =
         (subs, Array.zip terms1 terms2)
             ||> Seq.tryFold (fun acc (term1'', term2'') ->
-                unifyTerms term1'' term2'' acc)
+                tryUnifyTerms term1'' term2'' acc)
 
+    /// Tries to unify two atomic formulas.
     let unify formula1 formula2 =
         match (formula1, formula2) with
             | Formula (predicate1, terms1), Formula (predicate2, terms2)
                 when predicate1 = predicate2 ->
-                    unifyTermArrays terms1 terms2 List.empty
+                    tryUnifyTermArrays terms1 terms2 List.empty
                         |> Option.map List.rev
             | _ -> None
