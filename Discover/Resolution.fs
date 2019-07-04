33d52a8fe35cb0e794cc6fcef61da33cc3154b79
Brian Berns <brianberns@gmail.com>
2019-07-04 02:37:46 -0400

    Fixed factoring usage.

--- Discover/Resolution.fs
+++ Discover/Resolution.fs
@@ -51,46 +51,35 @@ module Resolution =
                 | LiteralNot (predicate, terms) ->
                     deconflictPredicate predicate terms Not)
 
-    /// Reduces the given clause to its smallest factor.
-    let private factor clause =
-
-        let rec loop (Clause literals as clause) =
+    /// Answers all factors of the given clause (including itself).
+    let rec private getFactors (Clause literals as clause) =
+        let combinations =
             literals
                 |> Seq.toList
                 |> List.combinations 2
-                |> Seq.tryPick (function
-
-                        // try to unify each pair of literals in the clause
+        seq {
+            yield clause
+            for combination in combinations do
+                match combination with
                     | (literal1 :: literal2 :: []) ->
-                        Unfiy.tryUnify literal1 literal2
-                            |> Option.map (fun subs ->
-
-                                    // reduce the entire clause using the successful substitution
-                                    // and recurse to see if further factoring is possible
-                                clause
+                        match Unfiy.tryUnify literal1 literal2 with
+                            | Some subst ->
+                                yield! clause
                                     |> Clause.map (
-                                        Substitution.applyLiteral subs)
-                                    |> loopDefault)
-
-                    | _ -> failwith "Unexpected")
-
-        and loopDefault clause =
-            clause
-                |> loop
-                |> Option.defaultValue clause
-
-        clause |> loopDefault
+                                        Substitution.applyLiteral subst)
+                                    |> getFactors
+                            | None -> ()
+                    | _ -> failwith "Unexpected"
+        }        
 
     /// Derives a conclusion from the given clauses using the resolution
     /// principle.
     let resolve clause1 clause2 =
 
-        let (Clause literals1) as clause1' =
-            clause1 |> factor
-        let (Clause literals2) =
-            clause2
-                |> deconflict clause1'
-                |> factor
+        let clause2 = deconflict clause1 clause2
+
+        let clauses1 = clause1 |> getFactors
+        let clauses2 = clause2 |> getFactors
 
         let extract = function
             | LiteralAtom _ as literal ->
@@ -99,20 +88,22 @@ module Resolution =
                 Formula predTerms |> Literal.ofFormula, -1
 
         seq {
-            for literal1 in literals1 do
-                let others1Lazy = lazy literals1.Remove(literal1)
-                for literal2 in literals2 do
-                    match extract literal1, extract literal2 with
-                        | (lit1, sign1), (lit2, sign2) when sign1 <> sign2 ->
-                            match Unfiy.tryUnify lit1 lit2 with
-                                | Some subs ->
-                                    let others2 = literals2.Remove(literal2)
-                                    yield Seq.append others1Lazy.Value others2
-                                        |> Seq.map (Substitution.applyLiteral subs)
-                                        |> set
-                                        |> Clause
-                                | None -> ()
-                        | _ -> ()
+            for (Clause literals1) in clauses1 do
+                for (Clause literals2) in clauses2 do
+                    for literal1 in literals1 do
+                        let others1Lazy = lazy literals1.Remove(literal1)
+                        for literal2 in literals2 do
+                            match extract literal1, extract literal2 with
+                                | (lit1, sign1), (lit2, sign2) when sign1 <> sign2 ->
+                                    match Unfiy.tryUnify lit1 lit2 with
+                                        | Some subst ->
+                                            let others2 = literals2.Remove(literal2)
+                                            yield Seq.append others1Lazy.Value others2
+                                                |> Seq.map (Substitution.applyLiteral subst)
+                                                |> set
+                                                |> Clause
+                                        | None -> ()
+                                | _ -> ()
         } |> set
 
 /// A resolution derivation.
