8b4ed601c68ca148da630da50a02db804d0c9d35
Brian Berns <brianberns@gmail.com>
2019-07-11 22:48:57 -0400

    Removed invalid assumption. See https://math.stackexchange.com/questions/3290370/first-order-logic-and-peano-arithmetic-paradox.

--- Bernsrite.Flogic/LinearInduction.fs
+++ Bernsrite.Flogic/LinearInduction.fs
@@ -97,19 +97,9 @@ module LinearInduction =
 
 module Strategy =
 
-    let tryProveImplication premises = function
-        | Implication (formula1, formula2) ->
-            let premises' =
-                seq {
-                    yield! premises
-                    yield formula1
-                }
-            LinearResolution.tryProve premises' formula2
-        | goal -> LinearResolution.tryProve premises goal
-
     /// Tries to prove the given formula.
     let tryProve language =
         Prover.serial [|
-            LinearInduction.tryProve language tryProveImplication
-            tryProveImplication
+            LinearInduction.tryProve language LinearResolution.tryProve
+            LinearResolution.tryProve
         |]
