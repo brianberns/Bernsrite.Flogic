3412058fe1d55aeac55d761a8386e23be8f85039
Brian Berns <brianberns@gmail.com>
2019-08-06 17:17:10 -0400

    Switched to commutative problem case.

--- Bernsrite.Flogic/Program.fs
+++ Bernsrite.Flogic/Program.fs
@@ -9,9 +9,8 @@ module Program =
 
         let dtStart = DateTime.Now
         let proofOpt =
-            "∀x.=(+(0,x), x)"
-            // "(=(+(0,a), a) -> =(+(0,s(a)), s(a)))"
-                |> Language.parse Peano.language
+            "=(+(0, y), +(y, 0)) ⇒ =(+(0, s(y)), +(s(y), 0))"
+                |> Language.parse Peano.theory.Language
                 |> System.tryProve Peano.theory
         printfn "%A" proofOpt
         printfn "%A" (DateTime.Now - dtStart)
